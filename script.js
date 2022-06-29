c6b05c235cce95e3100e6c40395778ecf3648596
Bo <omuqqz@gmail.com>
2022-06-29 19:25:25 +0300

    complete main tasks

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let sw = document.querySelector('#switch');
+let fromInput = document.querySelector('#from-wrapper .input-field');
+let toInput = document.querySelector('#to-wrapper .input-field');
+let toList = document.querySelector('#to-currency-list'),
+  fromList = document.querySelector('#from-currency-list'),
+  baseList = document.querySelector('#basic-currency-list');
 let rates;
 let currencies = [];
 let defToCurrency = 'RUB';
 let sourceBaseCurrency;
 let apiUrl = 'https://cdn.cur.su/api/nbu.json';
+let keyUpEvent = new Event('keyup');
+let terminalButton = document.querySelector('#terminal-button')
+
 window.addEventListener('load', () => {
   fetch(apiUrl)
     .then((response) => {
@@ -12,26 +19,49 @@ window.addEventListener('load', () => {
     .then((data) => {
       rates = data.rates;
       sourceBaseCurrency = data.base;
-      console.log(sw);
       setAllData();
     });
 });
 
-sw.addEventListener('click', () => {
-  sw.classList.toggle('rotate');
-  switchCurrencyList();
+fromInput.addEventListener('mouseup', () => {
+  toInput.value = convertCurrency(fromList.value, fromInput.value, toList.value);
 });
-
-sw.addEventListener('animationend', () => {
-  sw.classList.remove('rotate');
+fromInput.addEventListener('keyup', () => {
+  toInput.value = convertCurrency(fromList.value, fromInput.value, toList.value);
+});
+toInput.addEventListener('mouseup', () => {
+  fromInput.value = convertCurrency(toList.value, toInput.value, fromList.value);
+});
+toInput.addEventListener('keyup', () => {
+  fromInput.value = convertCurrency(toList.value, toInput.value, fromList.value);
+});
+fromList.addEventListener('change', () => {
+  fromInput.dispatchEvent(keyUpEvent);
+});
+toList.addEventListener('change', () => {
+  fromInput.dispatchEvent(keyUpEvent);
+});
+baseList.addEventListener('change', () => {
+  setBasicCurrency(baseList.value);
+  createRates(baseList.value);
+})
+terminalButton.addEventListener('click', () => {
+  let terminal = document.querySelector('#terminal');
+  completeCommand(terminal.value);
+  terminal.value = '';
 });
 
 function setAllData() {
-  let baseCurrency = getBaseCurrency();
+  let baseCurrency = getBasicCurrency();
   setSortedCurrencies(rates);
   createRates(baseCurrency);
   setFromCurrencyList(baseCurrency);
-  setToCurrencyList(defToCurrency);
+  if (baseCurrency == defToCurrency) {
+    setToCurrencyList('USD');
+  } else {
+    setToCurrencyList(defToCurrency);
+  }
+  setCurrencyList(baseCurrency, baseList);
 }
 
 function setSortedCurrencies(rates) {
@@ -41,17 +71,22 @@ function setSortedCurrencies(rates) {
   currencies.sort();
 }
 
-function getBaseCurrency() {
+function getBasicCurrency() {
   if (localStorage.getItem('baseCurrency') == undefined) {
     localStorage.setItem('baseCurrency', sourceBaseCurrency);
   }
   return localStorage.getItem('baseCurrency');
 }
 
+function setBasicCurrency(currency) {
+  localStorage.setItem('baseCurrency', currency);
+}
+
 function createRates(baseCurrency) {
   let ratesBaseCurrency = document.querySelector('.rates-label .basic-currency');
   ratesBaseCurrency.textContent = baseCurrency;
   let rateList = document.querySelector('.rates-list');
+  rateList.innerHTML = '';
   for (curr of currencies) {
     if (curr != baseCurrency) {
       rateList.append(createRate(curr, baseCurrency));
@@ -76,8 +111,7 @@ function createRate(currency, baseCurrency) {
     return rate;
 }
 
-function setCurrencyList(selectedCurrency, listSelector) {
-  let list = document.querySelector(listSelector);
+function setCurrencyList(selectedCurrency, list) {
   list.innerHTML = '';
   for (currency of currencies) {
     if (currency === selectedCurrency) {
@@ -89,22 +123,44 @@ function setCurrencyList(selectedCurrency, listSelector) {
 }
 
 function setFromCurrencyList(selectedCurrency) {
-  setCurrencyList(selectedCurrency, '#from-currency-list');
+  setCurrencyList(selectedCurrency, fromList);
 }
 
 function setToCurrencyList(selectedCurrency) {
-  setCurrencyList(selectedCurrency, '#to-currency-list');
+  setCurrencyList(selectedCurrency, toList);
 }
 
 function createOption(isSelected, content) {
   return `<option value="${content}" ${isSelected ? 'selected' : ''}>${content}</option>\n`;
 }
 
-function switchCurrencyList() {
-  let toList = document.querySelector('#to-currency-list'),
-    fromList = document.querySelector('#from-currency-list');
-  let toValue = toList.value,
-    fromValue = fromList.value;
-  setToCurrencyList(fromValue);
-  setFromCurrencyList(toValue);
+function convertCurrency(fromCurrency, fromAmount, toCurrency) {
+  return (rates[toCurrency] / rates[fromCurrency] * fromAmount).toFixed(3);
+}
+
+function completeCommand(command) {
+  let parts = command.split(' ').filter(el => el != '');
+  if (parts.length != 4) {
+    return;
+  }
+  if (typeof(parseFloat(parts[0])) != 'number') {
+    console.log('Not number');
+    return;
+  }
+  if (!checkCurrency(parts[1].toUpperCase())) {
+    alert(`Unknown currency \'${parts[1]}\'`);
+    return;
+  }
+  if (!checkCurrency(parts[3].toUpperCase())) {
+    alert(`Unknown currency \'${parts[3]}\'`);
+    return;
+  }
+  setFromCurrencyList(parts[1].toUpperCase());
+  setToCurrencyList(parts[3].toUpperCase());
+  fromInput.value = parts[0];
+  fromInput.dispatchEvent(keyUpEvent);
+}
+
+function checkCurrency(currency) {
+  return currencies.indexOf(currency) < 0 ? false : true;
 }
